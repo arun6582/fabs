78ec52093ff29c19c38fb3370ae0d4452ee2ee3b
arun6582 <arun@coverfoxmail.com>
2020-03-26 03:46:33 +0530

    added fabric to install requires

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
 setup(
-  name='fabs',
-  packages=find_packages(),
-  version='1.0',
-  author='Arun',
-  author_email='arun6582@gmail.com',
-  classifiers=[
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Environment :: Web Environment',
-  ],
+    name='fabs',
+    packages=find_packages(),
+    version='1.0',
+    author='Arun',
+    author_email='arun6582@gmail.com',
+    install_requires=[
+        'fabric>=2.5.0'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Environment :: Web Environment',
+    ],
 )
