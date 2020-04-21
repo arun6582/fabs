a3d1f3e6f44a77d23d0e4fcf16e2b04cab967185
mac-air-17 <arun6582@gmail.com>
2020-04-22 04:41:34 +0530

    link fabs

---
+++ link_fabs.sh
@@ -0,0 +1,5 @@
+ln -fs `pwd`/fabs/mac/system_tasks.py ~/tasks.py
+ln -fs `pwd`/fabs/mac/invoke.yaml ~/invoke.yaml
+
+ln -fs `pwd`/fabs/ubuntu/system_fabfile.py ~/fabric.py
+ln -fs `pwd`/fabs/ubuntu/fabric.yaml ~/fabric.yaml
