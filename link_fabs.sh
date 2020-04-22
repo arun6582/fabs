5c8fa05f608e39c97871c68f9d1d9ce6079feadc
arun6582 <arun@coverfoxmail.com>
2020-04-22 11:08:47 +0530

    link

--- link_fabs.sh
+++ link_fabs.sh
@@ -1,5 +1,5 @@
 curl https://raw.githubusercontent.com/arun6582/fabs/master/fabfile.py --output $HOME/fabfile.py
 curl https://raw.githubusercontent.com/arun6582/fabs/master/fabs/ubuntu/fabric.yaml --output $HOME/fabric.yaml
 
-curl https://raw.githubusercontent.com/arun6582/fabs/master/tasks.py --output $HOME/tasks.py --output $HOME/tasks.py
+curl https://raw.githubusercontent.com/arun6582/fabs/master/tasks.py --output $HOME/tasks.py
 curl https://raw.githubusercontent.com/arun6582/fabs/master/fabs/mac/invoke.yaml --output $HOME/invoke.yaml
