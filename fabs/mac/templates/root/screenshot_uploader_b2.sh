c66ed2b177ce883bc1807fff2de5b3a0317be877
Arun Kumar <arun6582@gmail.com>
2020-08-08 00:40:40 +0530

    automatically convert mov to mp4 before uploading

--- fabs/mac/templates/root/screenshot_uploader_b2.sh
+++ fabs/mac/templates/root/screenshot_uploader_b2.sh
@@ -1,6 +1,12 @@
 echo "`date` Screenshot uploader initiated"
 sleep .25s
 file_name=`ls -Art $1| tail -n 1`
+if [[ $file_name  == *"mov" ]]
+then
+  avconvert -s "$1/$file_name" -o "$1/$file_name.mp4" -p PresetPassthrough
+  file_name=$file_name.mp4
+fi
+  
 $B2_PATH authorize-account
 address=`$B2_PATH upload-file $B2_BUCKET "$1/$file_name" "snaps/$file_name" | $GREP -oP 'URL by file name: https.*'`
 echo $address | $GREP -oP 'https.*' | pbcopy
